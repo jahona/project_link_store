278edf6fecdb897e83de519bbae4eb65420fec75
jahona <jahona5240@gmail.com>
2021-06-29 16:38:40 +0900

    rpush data of link to redis of list data type

--- src/router/page.ts
+++ src/router/page.ts
@@ -11,7 +11,12 @@ router.get('/',
 
 router.post('/', 
     async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-        res.send('/');
+        const rData: any = {};
+        rData.link = req.body.link;
+        rData.timestamp = Date.now();
+        redisClient.RPUSH('crawling_list', JSON.stringify(rData));
+        
+        res.status(200).json({result: 'success'});
 });
 
 export default router;
